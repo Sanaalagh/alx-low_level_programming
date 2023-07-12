b2975b18fc33edbb6330f0a44a5d94e819799e33
Sanaalagh <Sanaalaghzou@gmail.com>
2023-07-11 19:09:36 -0700

    all tasks

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,98 +1,91 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
- * mem_clear - Clears the memory of a string array.
- * @s: The string array.
- */
-void mem_clear(char **s)
-{
-	int i;
-
-	for (i = 0; s[i] != NULL; i++)
-		free(s[i]);
-
-	free(s);
-}
-
-/**
- * word_count - Counts the number of words in a string.
+ * count_words - Counts the number of words in a string.
  * @str: The input string.
  *
  * Return: The number of words.
  */
-int word_count(char *str)
+int count_words(char *str)
 {
 	int count = 0;
-	int in_word = 0;
 	int i = 0;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] == ' ')
-			in_word = 0;
-		else if (in_word == 0)
-		{
-			in_word = 1;
-			count++;
-		}
+		/* Skip leading spaces */
+		while (str[i] == ' ')
+			i++;
 
-		i++;
+		if (str[i] != '\0')
+		{
+			count++;
+			/* Move to the end of the current word */
+			while (str[i] != ' ' && str[i] != '\0')
+				i++;
+		}
 	}
 
-	return (count);
+	return count;
 }
 
 /**
  * strtow - Splits a string into words.
  * @str: The input string.
  *
- * Return: A pointer to an array of words, or NULL on failure.
+ * Return: A pointer to an array of strings (words), or NULL on failure.
  */
 char **strtow(char *str)
 {
 	char **words;
 	int num_words;
-	int word_start = 0;
-	int word_end;
-	int word_len;
-	int i, j;
+	int i, j, k;
+	int word_start, word_end;
 
 	if (str == NULL || *str == '\0')
-		return (NULL);
+		return NULL;
 
-	num_words = word_count(str);
+	num_words = count_words(str);
 
 	words = malloc((num_words + 1) * sizeof(char *));
 	if (words == NULL)
-		return (NULL);
+		return NULL;
 
-	for (i = 0, j = 0; str[i] != '\0' && j < num_words; i++)
+	i = 0;
+	j = 0;
+	while (str[i] != '\0' && j < num_words)
 	{
-		if (str[i] != ' ')
+		/* Skip leading spaces */
+		while (str[i] == ' ')
+			i++;
+
+		/* Find the start and end of the current word */
+		word_start = i;
+		while (str[i] != ' ' && str[i] != '\0')
+			i++;
+		word_end = i - 1;
+
+		/* Allocate memory for the current word */
+		words[j] = malloc((word_end - word_start + 2) * sizeof(char));
+		if (words[j] == NULL)
 		{
-			word_start = i;
-			word_end = i;
-			while (str[word_end] != ' ' && str[word_end] != '\0')
-				word_end++;
-
-			word_len = word_end - word_start;
-			words[j] = malloc((word_len + 1) * sizeof(char));
-			if (words[j] == NULL)
-			{
-				mem_clear(words);
-				return (NULL);
-			}
-
-			for (i = 0; i < word_len; i++)
-				words[j][i] = str[word_start++];
-
-			words[j][i] = '\0';
-			j++;
+			for (k = 0; k < j; k++)
+				free(words[k]);
+			free(words);
+			return NULL;
 		}
+
+		/* Copy the characters of the current word */
+		for (k = 0; k <= word_end - word_start; k++)
+			words[j][k] = str[word_start + k];
+		words[j][k] = '\0';
+
+		j++;
 	}
 
 	words[j] = NULL;
 
-	return (words);
+	return words;
 }
