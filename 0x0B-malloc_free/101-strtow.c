cfcec12d26c33c8e4f33683f426fdec43e1183e0
Sanaalagh <Sanaalaghzou@gmail.com>
2023-07-11 19:37:16 -0700

    all tasks

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * number - function to calculate number of words
  * @str: string being passed to check for words
@@ -11,7 +10,6 @@
 int number(char *str)
 {
 	int a, num = 0;
-
 	for (a = 0; str[a] != '\0'; a++)
 	{
 		if (*str == ' ')
@@ -36,7 +34,6 @@ void free_everything(char **string, int i)
 		free(string[--i]);
 	free(string);
 }
-
 /**
  * strtow - function that splits string into words
  * @str: string being passed
@@ -46,7 +43,6 @@ char **strtow(char *str)
 {
 	int total_words = 0, b = 0, c = 0, length = 0;
 	char **words, *found_word;
-
 	if (str == 0 || *str == 0)
 		return (NULL);
 	total_words = number(str);
