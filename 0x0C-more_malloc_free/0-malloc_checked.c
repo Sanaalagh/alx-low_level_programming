96b0e2c8900be850e6d98e44716aeb40994a93e8
Sanaalagh <Sanaalaghzou@gmail.com>
2023-07-13 20:07:57 -0700

    all tasks

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,18 @@
+#include "main.h" 
+#include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * malloc_checked - creates an array of integers.
+ * @b: input.
+ * Return: Output.
+ */
 void *malloc_checked(unsigned int b)
 {
-    void *ptr = malloc(b);
+	void *g = malloc(b);
 
-    if (ptr == NULL)
-        exit(98);
-
-    return ptr ;
-    }
+	if (g == NULL)
+	{
+		exit(98);
+	}
+	return (g);
+}
