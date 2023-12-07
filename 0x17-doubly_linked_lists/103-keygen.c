3c90681f24efda6b844a0e8ada08c90f15ee87db
Sanaalagh <Sanaalaghzou@gmail.com>
2023-12-07 03:30:55 -0800

    0x17. C - Doubly linked lists

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,46 +1,53 @@
 #include <stdio.h>
-/**
- * main - find palindrome of largest product of two 3-digit numbers
- * Return: Always 1
- */
-int main(void)
-{
-	int product, max = 0, i, j, c = 0;
-	int numDigits = 6;
-	int d[6] = { 0 };
+#include <stdlib.h>
+#include <string.h>
 
-	for (i = 999; i > 100; i--)
+/**
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
+
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
+	tmp = (len ^ 59) & 63;
+	password[0] = codex[tmp];
+
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += argv[1][i];
+	password[1] = codex[(tmp ^ 79) & 63];
+
+	tmp = 1;
+	for (i = 0; i < len; i++)
+		tmp *= argv[1][i];
+	password[2] = codex[(tmp ^ 85) & 63];
+
+	tmp = 0;
+	for (i = 0; i < len; i++)
 	{
-		for (j = 999; j > 100; j--)
-		{
-			product = i * j;
-			c = 0;
-			d[0] = d[1] = d[2] = d[3] = d[4] = d[5] = 0;
-			while (product)
-			{
-				d[c] = product % 10;
-				product /= 10;
-				c++;
-			}
-			product = i * j;
-			numDigits = c;
-			if (numDigits == 6)
-			{
-				if ((d[0] == d[5]) &&
-				    (d[1] == d[4]) &&
-				    (d[2] == d[3]))
-					if (product > max)
-						max = product;
-			}
-			else if (numDigits == 5)
-			{
-				if ((d[0] == d[4]) &&
-				    (d[1] == d[3]))
-					if (product > max)
-						max = product;
-			}
-		}
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
 	}
-	printf("%d\n", max);
-	return (1);
+	srand(tmp ^ 14);
+	password[3] = codex[rand() & 63];
+
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(tmp ^ 239) & 63];
+
+	for (i = 0; i < argv[1][0]; i++)
+		tmp = rand();
+	password[5] = codex[(tmp ^ 229) & 63];
+
+	password[6] = '\0';
+	printf("%s", password);
+	return (0);
 }
