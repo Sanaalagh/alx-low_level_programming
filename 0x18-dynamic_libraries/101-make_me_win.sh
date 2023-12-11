a3d95271eaf83a250e324dd9d84324f8e66d498f
Sanaalagh <Sanaalaghzou@gmail.com>
2023-12-11 09:03:49 -0800

    some changes

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/Sanaalagh/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD="$PWD/../libtest.so"
+wget -P /tmp  https://github.com/Sanaalagh/alx-low_level_programming/raw/main/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
